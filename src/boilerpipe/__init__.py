1657124e77b2395366146abd8a7acd619cbf94e9
Misja Hoebe <misja.hoebe@gmail.com>
2012-06-22 12:05:30 +0200

    Use system path separator, see issue #3

--- src/boilerpipe/__init__.py
+++ src/boilerpipe/__init__.py
@@ -7,4 +7,4 @@ if jpype.isJVMStarted() != True:
     for top, dirs, files in os.walk(imp.find_module('boilerpipe')[1]+'/data'):
         for nm in files:       
             jars.append(os.path.join(top, nm))
-    jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=%s" % ':'.join(jars))
+    jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=%s" % os.pathsep.join(jars))
