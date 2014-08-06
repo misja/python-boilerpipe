e4263899e2addf812793475e937535490c384d3d
rshiva <nadarshiva.r@gmail.com>
2014-08-06 18:04:46 +0530

    removed print statements

--- src/boilerpipe/__init__.py
+++ src/boilerpipe/__init__.py
@@ -2,11 +2,9 @@ import os
 import imp
 import jpype
 
-print "jpype.isJVMStarted()  ",jpype.isJVMStarted()
 if jpype.isJVMStarted() != True:
     jars = []
     for top, dirs, files in os.walk(imp.find_module('boilerpipe')[1]+'/data'):
         for nm in files:       
             jars.append(os.path.join(top, nm))
-    print "---->",jpype.startJVM(jpype.getDefaultJVMPath()),"==> path ",jpype.getDefaultJVMPath()        
     jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=%s" % os.pathsep.join(jars))
